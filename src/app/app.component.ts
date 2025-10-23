f438e6f8da6ac745940f47accb6efa40c8e371a8
andrzejW <witkowskiandrzej8@gmail.com>
2025-10-23 10:16:54 +0200

    feat: add logo, offers, and bookings components with basic templates and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LogoComponent } from "./logo/logo.component";
+import { CommonModule } from '@angular/common';
+import { OffersComponent } from './offers/offers.component';
+import { BookingsComponent } from './bookings/bookings.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    LogoComponent,
+    OffersComponent,
+    BookingsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
