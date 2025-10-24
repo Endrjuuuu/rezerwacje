b8f503e11e7d7fb011d02c8d0bfa5fb0a9202523
AndrzejW <witkowskiandrzej8@gmail.com>
2025-10-24 15:29:44 +0200

    feat: enhance bookings feature with service integration, data fetching, and updated UI

--- src/app/bookings.service.ts
+++ src/app/bookings.service.ts
@@ -1,10 +1,31 @@
 import { Injectable } from '@angular/core';
-import { OffersService } from './offers.service';
+import { Offer, OffersService } from './offers.service';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export type Booking = {
+  id: string;
+  name: string;
+  adults: number;
+  children: number;
+  offer: Offer;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookingsService {
+  url = "http://localhost:3000/booking";
 
-  constructor(private offersService: OffersService) { }
+  constructor(
+    private http: HttpClient,
+    private offersService: OffersService) { }
+
+  getBookings(): Observable<Booking[]> {
+    return this.http.get<Booking[]>(this.url);
+  }
+
+  addBooking(data: Booking): Observable<any> {
+    return this.http.post<Booking>(this.url, data);
+  }
 }
--- src/app/bookings/bookings.component.ts
+++ src/app/bookings/bookings.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
-import { BookingsService } from '../bookings.service';
+import { Component, OnInit } from '@angular/core';
+import { Booking, BookingsService } from '../bookings.service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-bookings',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './bookings.component.html',
   styleUrl: './bookings.component.scss'
 })
-export class BookingsComponent {
+export class BookingsComponent implements OnInit {
+
+  bookings: Booking[] = [];
 
   constructor(private bookingsService: BookingsService) {
 
+  }
+  ngOnInit(): void {
+    this.bookingsService.getBookings().subscribe({
+      error: () => {
+        console.log("dane nie zostały pobrane");
+      },
+      next: (data: Booking[]) => {
+        this.bookings = data
+      }
+    });
   }
 }
