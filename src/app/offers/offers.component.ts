12b8e1277cd256df9d5ccc0720795c34b062738a
andrzejW <witkowskiandrzej8@gmail.com>
2025-10-24 08:39:43 +0200

    feat: enhance offers component with structured offer display and styling

--- src/app/offers/offers.component.ts
+++ src/app/offers/offers.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Offer, OffersService } from '../offers.service';
+import { OfferComponent } from '../offer/offer.component';
 
 @Component({
   selector: 'app-offers',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, OfferComponent],
   templateUrl: './offers.component.html',
   styleUrl: './offers.component.scss'
 })
